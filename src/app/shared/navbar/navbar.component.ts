2b9f149cb5a0ed8d4d5e4c07b051d7df3e1af1a3
Charles Atienza <charles.atienza@yahoo.com>
2024-12-21 11:19:47 -0800

    fixed navbar, added bootstrap and primeng

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,12 +3,12 @@ import { ButtonModule } from "primeng/button";
 import { AvatarModule } from "primeng/avatar";
 import { ToolbarModule } from "primeng/toolbar";
 import { Router } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StyleClassModule } from "primeng/styleclass";
 
 @Component({
   selector: "app-navbar",
   standalone: true,
-  imports: [ButtonModule, AvatarModule, ToolbarModule, NgbModule],
+  imports: [ButtonModule, AvatarModule, ToolbarModule, StyleClassModule],
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
