0b312af2439f50c23f073e606f4e663d8d4a07da
Charles Atienza <charles.atienza@yahoo.com>
2024-12-25 11:17:47 -0800

    complete home page

--- src/app/home-page/home-page-main-content/home-page-main-content.component.ts
+++ src/app/home-page/home-page-main-content/home-page-main-content.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RadioCardButtonComponent } from "../../shared/components/radio-card-button/radio-card-button.component";
 import { DifficultyImagePath } from "../../shared/enums/difficulty-image-path.enum";
+import { ICardButton } from "../../shared/models/card-button.model";
 
 @Component({
   selector: "app-home-page-main-content",
@@ -12,4 +13,30 @@ import { DifficultyImagePath } from "../../shared/enums/difficulty-image-path.en
 })
 export class HomePageMainContentComponent {
   DifficultyImagePath = DifficultyImagePath;
+  cardButtons: ICardButton[] = [
+    {
+      title: "Basic",
+      isComingSoon: false,
+      logoURI: DifficultyImagePath.BASIC,
+      onCardClick: () => {},
+    },
+    {
+      title: "Intermediate",
+      isComingSoon: true,
+      logoURI: DifficultyImagePath.INTERMEDIATE,
+      onCardClick: () => {},
+    },
+    {
+      title: "Advanced",
+      isComingSoon: true,
+      logoURI: DifficultyImagePath.ADVANCED,
+      onCardClick: () => {},
+    },
+    {
+      title: "???",
+      isComingSoon: true,
+      logoURI: DifficultyImagePath.RIP,
+      onCardClick: () => {},
+    },
+  ];
 }
--- src/app/shared/components/radio-card-button/radio-card-button.component.ts
+++ src/app/shared/components/radio-card-button/radio-card-button.component.ts
@@ -14,6 +14,7 @@ import { CommonModule } from "@angular/common";
 })
 export class RadioCardButtonComponent {
   @Input() cardButtons: ICardButton[] = [];
+  @Input() isHorizontalSpread: boolean = false;
 
   onCardClick($event: Event, cardButtonIndex: number) {
     const selectedCardButtons = this.cardButtons[cardButtonIndex];
